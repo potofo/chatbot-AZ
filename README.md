c2fd29c2dd453c82137afacd75c0e2c253a557e3
Tsutomu Nagura <nagura@d6.dion.ne.jp>
2023-10-01 22:03:05 +0900

    Fixed incremental response from assustant.

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# chatbot-AZ
- Simple Chatbot program for Azure OpenAI Service
+# chatbot-AZ - Simple Chatbot program for Azure OpenAI Service
+
+## Description
+This is a sample program designed to help inexperienced programmers learn how to use the Azure OpenAI Service API. It demonstrates how to interact with the OpenAI Chat Completion API and handle conversation persistence.
+
+## Specifications of the Chatbot
+
+- The tokens include the last 5 conversations between the user and the assistant, including the latest one. This ensures continuity of the conversation and makes it appear as if the conversation is ongoing.
+- The system prompt is fixed as follows:
+
+```python
+**As an experienced engineer, you will step-by-step consider complex technical problems, answer questions and provide advice.**
+
+```
+
+- The token limit size for the prompt is set to 800 tokens in the API instructions. If this limit is exceeded, an error is expected.
+- API calls are intended to be asynchronous, with the response messages being streamed in real-time to the response container in Streamlit. However, there is a bug that prevents displaying while communicating asynchronously.
+
+## Code explanation
+This code is a simple chatbot program for Azure OpenAI Service. Here is an explanation of the different sections of the code:
+
+- Import sections: This section imports the necessary modules and packages for the program, including openai, streamlit, and pprint (for debugging).
+
+- Global definitions: This section defines global variables and constants used in the program, such as the maximum number of messages, the Azure OpenAI API parameters, the deployment ID, and the system prompt.
+
+- Global Variables definitions: This section defines two dictionaries, 'dict_message' and 'dict_response', which are used to store the user input messages and the chatbot responses respectively.
+
+- Specify Azure OpenAI API parameters: This section sets the necessary parameters for the Azure OpenAI API, including the API type, API key, API host, and API version.
+
+- Screen Layout definitions: This section defines the layout of the chatbot interface using streamlit container elements. It includes containers for the response messages and the user input.
+
+- First Message: This section displays the first message from the chatbot in the response container.
+
+- form to container_input definitions: This section creates a form to capture the user's input prompt in the input container.
+
+- Restore list_messages from session state: This section checks if the list_messages variable exists in the session state (which persists across multiple interactions with the chatbot). If it exists, it restores the previous messages. Otherwise, it sets the initial system prompt message and saves it in the session state.
+
+- Event Dispatcher: This section handles the event when the user enters a prompt. It adds the user's prompt to the list of messages, displays all the messages in the response container, sends the messages to the Azure OpenAI Chat Completion API, and receives the completion in chunks. It then appends the chatbot's response to the list of messages, purges old messages if necessary, and saves the updated list of messages in the session state for future interactions.
+
+Note: This code is specifically designed to work with Azure OpenAI Service and Streamlit. It leverages the capabilities of these services to create a simple chatbot interface.
+
+## Requirements
+- Azure OpenAI Service:
+  - You will need an Azure OpenAI endpoint, API key, and API version for this program to work.
+  - A subscription to the pay-as-you-go Azure OpenAI Service is required for key issuance.
+- openai package (Ver 0.27.8):
+  - This program uses the `openai` package to interact with the OpenAI API.
+- Streamlit:
+  - The program requires Streamlit version 1.24.0 or higher to use `streamlit_chat`.
+
+## Establish OpenAI key on Azure OpenAI Service
+Here is the process for issuing an API in Azure OpenAI Service:
+
+1. Log in to the Azure portal.
+2. Create a resource group.
+3. Create Azure OpenAI Service within the resource group.
+4. Deploy LLM in Azure OpenAI Studio.
+5. Return to Azure OpenAI Service and retrieve the necessary information
+   (Endpoint, API key, API version).
+
+This is the process for issuing an API in Azure OpenAI Service. For detailed instructions, please refer to the official Azure documentation.
+
+## Author
+- potofo
+
+## Revision
+- 2023/09/30 01-00 potofo: Initial Creation.
+- 2023/10/01 01-01 portfo: Fixed incremental response from assistant.
+
+## Disclaimer
+Please be aware that we are not responsible for any problems caused by this program.
+
+## Restrictions
+-
+
+## License
+- MIT License
+
+## Copyright
+- Copyright (c) 2023 potofo. All rights reserved.
+
+## Note
+-
---
+++ chatbot-AZ.py
@@ -0,0 +1,151 @@
+################################################################################
+# Name:
+#   Simple Chatbot program for Azure OpenAI Service
+# Description:
+#   This is sample program for learning Open API for inexperienced programmers.
+# Requirement:
+#   about Azure OpenAI Service
+#     You will need an Azure OpenAI endpoint, API key, and API version for this
+#     program to work.
+#     A subscription to the pay-as-you-go Azure OpenAI Service is required for
+#     key issuance.
+#   openai
+#     Using the openai package(Ver 0.27.8) to use the OpenAI API
+#   Streamlit
+#     To use streamlit_chat, the streamlit version must be 1.24.0 or higher.
+# Auther:
+#   potofo
+# Revision:
+#   2023/09/30 01-00 potofo   Initial Creation.
+#   2023/10/01 01-01 portfo   Fixed incremental response from assustant.
+# Disclaimer:
+#   Please be aware that we are not responsible for any problems caused by this 
+#   program.
+# Restriction:
+#   -
+# License:
+#   MIT License
+# Copyright:
+#   Copyright (c) 2023 potofo. All rihtts reserved.
+# Note:
+#   -
+################################################################################
+# Import sections
+import openai              # 0.27.8
+import streamlit as st     # must be 1.24.0 or higher
+import pprint              # for debug to confirm message internal scheme 
+
+# Global definitions
+MAX_MESSAGES        = 5
+OPENAI_API_TYPE     = 'azure'
+OPENAI_API_KEY      = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+OPENAI_API_HOST     = 'https://xxxxxxxx.openai.azure.com/'
+OPENAI_API_VERSION  = '2023-07-01-preview'
+# The AZURE_DEPLOYMENT_ID is the name of the LLM deployed with
+# Azure OpenAI Service
+AZURE_DEPLOYMENT_ID = 'gpt-35-turbo'
+#AZURE_DEPLOYMENT_ID = 'gpt-4'
+SYSTEM_PROMPT       = 'As an experienced engineer, you will step-by-step' \
+                      ' consider complex technical problems, answer questions,' \
+                      ' and provide advice.'
+
+# Global Valiables definitions
+list_messages = []
+dict_message  = {}
+dict_response = {}
+
+# Specify Azure OpenAI API parameters
+openai.api_type    = OPENAI_API_TYPE
+openai.api_base    = OPENAI_API_HOST
+openai.api_version = OPENAI_API_VERSION
+openai.api_key     = OPENAI_API_KEY
+
+################################################################################
+# Screen Layout definitions
+# Layout is implemented with streamlit container elements.
+container_response = st.container()
+container_input    = st.container()
+
+# First Message
+block_first_message = container_response.chat_message('assistant')
+block_first_message.write('This is ChatGPT,May I help you?')
+
+# form to container_input definitions
+prompt = container_input.chat_input('prompt')
+
+# Restore list_messages from session state
+if 'list_messages' in st.session_state:
+    print('Restore list_message valiable successful from session status!!')
+    list_messages = st.session_state['list_messages']
+else:
+    print('Not Found list_message key in session status')
+    # Set System prompt
+    dict_message['content']  =  SYSTEM_PROMPT
+    dict_message['role']     = 'system'
+    list_messages.append(dict_message)
+    pprint.pprint(list_messages)
+    # Save Session state
+    st.session_state['list_messages'] = list_messages
+
+################################################################################
+# Event Dispatcher
+if(prompt):
+    # User prompt input event
+    print(f'Detect user prompt input event (prompt:{prompt})')
+
+    # Establish message to OpenAI Chat Completion API
+    dict_message['content']  = prompt
+    dict_message['role']     = 'user'
+    list_messages.append(dict_message)
+
+    # Output system prompts and prompts from User in response container.
+    for message in list_messages:
+        block_message = container_response.chat_message(message['role'])
+        block_message.write(message['content'])
+
+    # Send message to LLM with conversation persistence maintained.
+    # The stream parameter specifies asynchronous communication
+    completion = openai.ChatCompletion.create(
+        deployment_id     = AZURE_DEPLOYMENT_ID,
+        messages          = list_messages,
+        stream            = True,
+        temperature       = 0.7,
+        max_tokens        = 800,
+        top_p             = 0.95,
+        frequency_penalty = 0,
+        presence_penalty  = 0,
+        stop              = None
+    )
+
+    result_area = st.empty()
+    response_agent = ''
+
+    # In case of the stream=True parameter is specified in the ChatCompletion.
+    # create function, the completion is divided into chunks and received.
+    block_message = container_response.chat_message('assistant')
+    async_message = block_message.empty()
+
+    for chunk in completion:
+        if(chunk['id']):
+            next = chunk['choices'][0]['delta'].get('content', '')
+            response_agent += next
+            async_message.write(response_agent)
+
+    # Append Agent response to list_messages
+    dict_response['role']  = 'assistant'
+    dict_response['content']  = response_agent
+    list_messages.append(dict_response)
+    
+    # Purge old list_massages without system prompt.
+    num_messages = len(list_messages)
+    print(f'num_messages:{num_messages}')
+    pprint.pprint(list_messages)
+    if(num_messages > (MAX_MESSAGES * 2 + 1)):
+        # (user + assistant) x MAX_MESSAGES + system x 1
+        del list_messages[1]
+        del list_messages[1]
+
+    # Save Session state
+    # The streamlit is internally reloaded when it detects an event, 
+    # list_message must be set to session_state.
+    st.session_state['list_messages'] = list_messages
